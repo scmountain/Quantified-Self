175ae1f680099b05ff77f193fd1f6ffd2722a976
Spencer Carter <scmountain@gmail.com>
2017-05-06 15:10:02 -0600

    WIP POST test is failing

--- server.js
+++ server.js
@@ -1,17 +1,53 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var md5 = require('md5');
 
-app.set('port', process.env.PORT || 3000)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Quantified Self';
+app.locals.foods = {
+  wowowow: 'I am a banana'
+}
 
 app.get('/', function(request, response){
   response.send(app.locals.title);
 });
 
-if (!module.parent) {
-  app.listen(app.get('port'), function(){
-  console.log(app.locals.title + ' is running on ' + app.get('port'))
+app.get('/api/foods/:id', function(request, response){
+  var id = request.params.id
+  var message = app.locals.foods[id];
+
+  if(!message){ return response.sendStatus(404) }
+
+  response.json({ id: id, message: message});
 });
+
+app.post('/api/foods', function(request,response){
+  response.status(201).end();
+});
+
+app.post('/api/foods', function(request, response){
+  var id = Date.now()
+  var message = request.body.message
+
+  if (!message) {
+    return response.status(422).send({
+      error: "No message property provided"
+    });
+  }
+
+  app.locals.foods[id] = message
+ 
+  response.status(201).json({ id, message })
+});
+
+if(!module.parent) {
+  app.listen(app.get('port'), function(){
+    console.log(app.locals.title + ' is running on ' + app.get('port'))
+  });
 }
 
 module.exports = app;
--- test/server-test.js
+++ test/server-test.js
@@ -1,22 +1,24 @@
 var assert = require('chai').assert;
 var request = require('request');
-var app = require('../server');
+var app = require('../server.js');
+var pry = require('pryjs');
 
-describe('Server', function(){
+
+describe("Server", function(){
   before(function(done){
-    this.port = 9876;
-    this.server = app.listen(this.port, function(error, result){
-      if(error) { return done(error); }
-      done();
+      this.port = 9876
+      this.server = app.listen(this.port, function(err, result){
+        if (err) { return done(err); }
+        done();
+      });
+
+      this.request = request.defaults({
+          baseUrl: 'http://localhost:9876/'
+        });
     });
 
-    this.request = request.defaults({
-      baseUrl: 'http://localhost:9876/'
-    });
-
-    after(() => {
+  after(function(){
       this.server.close();
-    });
   });
 
   it('should exist', function(){
@@ -26,16 +28,61 @@ describe('Server', function(){
   describe('GET /', function(){
     it('should return a 200', function(done){
       this.request.get('/', function(error, response){
-        if (error) { done(error); }
+        if (error) { done(error) }
         assert.equal(response.statusCode, 200);
         done();
       });
     });
   });
 
-  describe('GET /api/secrets/:id', function(){
+  describe('GET /api/foods/:id', function(){
     beforeEach(function(){
-      app.locals.secrets = {}
-    })
-  })
+      app.locals.foods = {
+        wowowow: 'I am a banana'
+      }
+    });
+
+    it('should return a 404 if the resource is not found', function(done){
+      this.request.get('/api/foods/bahaha', function(error, response){
+        if(error) { done(error) }
+        assert.equal(response.statusCode, 404);
+        done();
+      });
+    });
+
+    it('should return a 200 if the resource is found', function(done){
+      this.request.get('/api/foods/wowowow', function(error, response){
+        if(error) { done(error) }
+        assert.equal(response.statusCode, 200);
+        assert(response.body.includes('wowowow'), 'ID was not included');
+        assert(response.body.includes('I am a banana'), 'ID was not included');
+          done();
+      });
+    });
+  });
+
+  describe('POST /api/foods', function(){
+    beforeEach(function(){
+      app.locals.foods = {}
+    });
+
+    it('should not return 404', function(done){
+      this.request.post('/api/foods', function(error, response){
+        if (error) { done(error) }
+        assert.notEqual(response.statusCode, 404)
+        done();
+      });
+    });
+
+    it('should recieve and store data', function(done){
+      var message = { message: 'I dont like Pineapples on my pizza'};
+
+      this.request.post('/api/foods', { form: message }, function(error, response){
+        if (error) { done(error); }
+        done();
+        var foodCount = Object.keys(app.locals.foods).length;
+        assert.equal(foodCount, 1, `expected 1 food, found ${foodCount}`);
+      });
+    });
+  });
 });
