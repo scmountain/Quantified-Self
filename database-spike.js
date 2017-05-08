c4c54d5fb18f5dd4308522df70273c8d0afa3c9f
Max Glassie <msglassie@gmail.com>
2017-05-08 17:19:06 -0600

    Builds out the database-spike file

--- database-spike.js
+++ database-spike.js
@@ -2,4 +2,11 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
-process.exist
+
+database.raw(`INSERT INTO foods (name, calories, created_at) VALUES (?, ?, ?)`, ['Sweet Baby Ray\'s BBQ', 95, new Date])
+.then(() => {
+    database.raw('SELECT * FROM foods').then((data) => {
+      console.log(data.rows)
+      process.exit();
+    })
+  })
