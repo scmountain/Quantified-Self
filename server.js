81a9204443129036b29caa924de1c1ef95c7fa92
Max Glassie <msglassie@gmail.com>
2017-05-11 13:55:39 -0600

    Post Request Completed
    The database variable was incorrect, so we've changed that and
    now all tests are passing including the POST request, which returns a
    201. We'll consider refactoring later to have it return data.

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const md5 = require('md5');
+const cors = require('cors');
+
+app.use(cors( { origin: '*' } ))
 
 const FoodsController = require('./lib/controllers/foods-controller')
 
@@ -16,18 +19,6 @@ app.use('/api/v1/foods/', FoodsController)
 app.get('/', function(request, response){
   response.send(app.locals.title);
 });
-
-// app.get('/api/foods/:id', function(request, response){
-//   FoodsController.show(request, response)
-// });
-
-// app.post('/api/foods', FoodsController.create);
-
-
-
-
-
-
 
 if(!module.parent) {
   app.listen(app.get('port'), function(){
